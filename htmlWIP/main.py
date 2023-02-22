43fe984ff1c03fe8440719a89d8689695d188620
Zack Doubikin <61536664+Megakarp@users.noreply.github.com>
2023-02-21 20:10:45 -0500

    logging in is now required in order view home and search function

--- htmlWIP/main.py
+++ htmlWIP/main.py
@@ -37,6 +37,7 @@ def login():
             msg = 'Logged in Successfully!'
             session['loggedin'] = True
             session['username'] = username
+            session['password'] = password
             return redirect(url_for('main'))
         else:
             msg = 'Username or password is Incorrect!'
@@ -108,33 +109,21 @@ def home():
 def main():
     # Output message if something goes wrong...
     msg = ''
-        # Check if "username" and "password" POST requests exist (user submitted form)
-    if request.method == 'POST' and 'username' in request.form and 'password' in request.form:
-        # Create variables for easy access
-        username = request.form['username']
-        password = request.form['password']
-        # Check if account exists using MySQL
-        with open('CSVFolder/userinfo.csv', mode="r") as d:
-            reader = csv.reader(d, delimiter=',')
-            for row in reader:
-                if row == [username,password]:
-                    msg = 'Logged in Successfully!'
-                    session['loggedin'] = True
-                    session['username'] = row['username']
-                    return redirect(url_for('home'))
-                else:
-                    msg = 'Username or password is Incorrect!'
-    #Show the login form with message (if any)
-    return render_template('main.html', msg='')
+    if 'loggedin' in session:
+        #Show the login form with message (if any)
+        return render_template('main.html', msg='')
+    # User is not loggedin redirect to login page
+    return redirect(url_for('login'))
 # http://localhost:5000/pythinlogin/profile - this will be the profile page, only accessible for loggedin users
 @app.route('/htmlWIP/profile')
 def profile():
     # Check if user is loggedin
     if 'loggedin' in session:
         # We need all the account info for the user so we can display it on the profile page
-        cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
-        cursor.execute('SELECT * FROM accounts WHERE id = %s', (session['id'],))
-        account = cursor.fetchone()
+        username = session['username']
+        password = session['password']
+        verify_login(username, password)
+        account = (username, password)
         # Show the profile page with account info
         return render_template('profile.html', account=account)
     # User is not loggedin redirect to login page
@@ -142,45 +131,47 @@ def profile():
 
 @app.route('/htmlWIP/search', methods=['GET','POST'])
 def search():
-    # sets app id and app key for API
-    # sign up at https://developer.edamam.com/edamam-recipe-api for id and key, and insert here
-    app_id = "47d553e8"
-    app_key = "b749a2fd18806cba1583c944bf24a546"
-    result=''
-    # defines variables to be used for including API parameters
-    includeAppId = "app_id={}".format(app_id)
-    includeAppKey = "app_key={}".format(app_key)
+    if 'loggedin' in session:
+        # sets app id and app key for API
+        # sign up at https://developer.edamam.com/edamam-recipe-api for id and key, and insert here
+        app_id = "47d553e8"
+        app_key = "b749a2fd18806cba1583c944bf24a546"
+        result=''
+        # defines variables to be used for including API parameters
+        includeAppId = "app_id={}".format(app_id)
+        includeAppKey = "app_key={}".format(app_key)
 
-        # POST request
-    if request.method == 'POST' and 'search' in request.form:
-        # asks user to enter ingredient(s)
-        ingredient = request.form['search']
-        while ingredient == "":
-            ingredient = input("You must enter at least one or more ingredients. Try again: ")
-        # use split and join functions to enable selection of more than one ingredient
-        ingredients = "q={}".format(ingredient)
-        # test
-        # print(ingredients)
-        url = 'https://api.edamam.com/auto-complete?{}&{}&{}'.format(ingredients, includeAppId, includeAppKey)
-        #recipeChoices = 'You searched for ingredient options, using {} '.format(ingredients)
+            # POST request
+        if request.method == 'POST' and 'search' in request.form:
+            # asks user to enter ingredient(s)
+            ingredient = request.form['search']
+            while ingredient == "":
+                ingredient = input("You must enter at least one or more ingredients. Try again: ")
+            # use split and join functions to enable selection of more than one ingredient
+            ingredients = "q={}".format(ingredient)
+            # test
+            # print(ingredients)
+            url = 'https://api.edamam.com/auto-complete?{}&{}&{}'.format(ingredients, includeAppId, includeAppKey)
+            #recipeChoices = 'You searched for ingredient options, using {} '.format(ingredients)
 
-        # requests and extracts recipes from the API, into the 'results' variable, based on user choices above
-        results = requests.get(url)
-        datas = json.loads(results.text)
-        data_list = []
-        for data in datas:
-            print(data)
-            data_list.append(data)
+            # requests and extracts recipes from the API, into the 'results' variable, based on user choices above
+            results = requests.get(url)
+            datas = json.loads(results.text)
+            data_list = []
+            for data in datas:
+                print(data)
+                data_list.append(data)
 
 
-        # Printing the results
-        # prints 'You've searched for {cuisineReq}, {dietReq} recipes, using {ingredient(s)}'
-        # based on user's choices/input
-        result = data_list
-    elif request.method == 'POST':
-        # Form is empty... (no POST data)
-        result = 'Please fill out the form!'
-    return render_template('search.html', result = result)
+            # Printing the results
+            # prints 'You've searched for {cuisineReq}, {dietReq} recipes, using {ingredient(s)}'
+            # based on user's choices/input
+            result = data_list
+        elif request.method == 'POST':
+            # Form is empty... (no POST data)
+            result = 'Please fill out the form!'
+        return render_template('search.html', result = result)
+    return redirect(url_for('login'))
 
 @app.route('/htmlWIP/search')
 def recipesearch():
