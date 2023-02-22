b13eee14b92b802a1cfce2501a05610c88973b82
Zack Doubikin <61536664+Megakarp@users.noreply.github.com>
2023-02-21 19:43:20 -0500

    login via csv works now

--- htmlWIP/main.py
+++ htmlWIP/main.py
@@ -4,6 +4,7 @@ import MySQLdb.cursors
 import re
 import requests
 import json
+import csv
 
 #more than likely need to pip install flask
 # and pip install
@@ -32,34 +33,43 @@ def login():
         # Create variables for easy access
         username = request.form['username']
         password = request.form['password']
-        # Check if account exists using MySQL
-        cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
-        cursor.execute('SELECT * FROM accounts WHERE username = %s AND password = %s', (username, password,))
-        # Fetch one record and return result
-        account = cursor.fetchone()
-        # If account exists in accounts table in out database
-        if account:
-            # Create session data, we can access this data in other routes
+        if verify_login(username, password):
+            msg = 'Logged in Successfully!'
             session['loggedin'] = True
-            session['id'] = account['id']
-            session['username'] = account['username']
-            # Redirect to home page
-            return redirect(url_for('home'))
+            session['username'] = username
+            return redirect(url_for('main'))
         else:
-            # Account doesnt exist or username/password incorrect
-            msg = 'Incorrect username/password!'
+            msg = 'Username or password is Incorrect!'
     #Show the login form with message (if any)
     return render_template('index.html', msg='')
+
+def verify_login(username, password):
+    with open('CSVFolder/userinfo.csv', 'r') as file:
+        reader = csv.reader(file)
+        for row in reader:
+            if row[0] == username and row[1] == password:
+                return True
+    return False
 
 # http://localhost:5000/python/logout - this will be the logout page
 @app.route('/htmlWIP/logout')
 def logout():
     # Remove session data, this will log the user out
    session.pop('loggedin', None)
-   session.pop('id', None)
    session.pop('username', None)
    # Redirect to login page
    return redirect(url_for('login'))
+
+def create_user(username, password):
+    with open('CSVFolder/userinfo.csv', 'r') as file:
+        reader = csv.reader(file)
+        for row in reader:
+            if row[0] == username:
+                return False
+    with open('CSVFolder/userinfo.csv', 'a', newline='') as file:
+        writer = csv.writer(file)
+        writer.writerow([username, password])
+    return True
 
 # http://localhost:5000/pythinlogin/register - this will be the registration page, we need to use both GET and POST requests
 @app.route('/htmlWIP/register', methods=['GET', 'POST'])
@@ -67,35 +77,23 @@ def register():
     # Output message if something goes wrong...
     msg = ''
     # Check if "username", "password" and "email" POST requests exist (user submitted form)
-    if request.method == 'POST' and 'username' in request.form and 'password' in request.form and 'email' in request.form:
+    if request.method == 'POST' and 'username' in request.form and 'password' in request.form and 'password2' in request.form:
         # Create variables for easy access
         username = request.form['username']
         password = request.form['password']
-        email = request.form['email']
-        # Check if account exists using MySQL
-        cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
-        cursor.execute('SELECT * FROM accounts WHERE username = %s', (username,))
-        account = cursor.fetchone()
-        # If account exists show error and validation checks
-        if account:
-            msg = 'Account already exists!'
-        elif not re.match(r'[^@]+@[^@]+\.[^@]+', email):
-            msg = 'Invalid email address!'
-        elif not re.match(r'[A-Za-z0-9]+', username):
-            msg = 'Username must contain only characters and numbers!'
-        elif not username or not password or not email:
-            msg = 'Please fill out the form!'
-        else:
-            # Account doesnt exists and the form data is valid, now insert new account into accounts table
-            cursor.execute('INSERT INTO accounts VALUES (NULL, %s, %s, %s)', (username, password, email,))
-            mysql.connection.commit()
-            msg = 'You have successfully registered!'
+        password2 = request.form['password2']
+        if password  == password2:
+            if not username or not password:
+                return 'Username and password are required'
+            if create_user(username, password):
+                return 'User created successfully'
+            else:
+                return 'User already exists'
     elif request.method == 'POST':
         # Form is empty... (no POST data)
         msg = 'Please fill out the form!'
     # Show registration form with message (if any)
     return render_template('register.html', msg=msg)
-
 # http://localhost:5000/pythinlogin/home - this will be the home page, only accessible for loggedin users
 @app.route('/htmlWIP/home')
 def home():
@@ -116,21 +114,17 @@ def main():
         username = request.form['username']
         password = request.form['password']
         # Check if account exists using MySQL
-        cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
-        cursor.execute('SELECT * FROM accounts WHERE username = %s AND password = %s', (username, password,))
-        # Fetch one record and return result
-        account = cursor.fetchone()
-        # If account exists in accounts table in out database
-        if account:
-            # Create session data, we can access this data in other routes
-            session['loggedin'] = True
-            session['id'] = account['id']
-            session['username'] = account['username']
-            # Redirect to home page
-            return redirect(url_for('home'))
-        else:
-            # Account doesnt exist or username/password incorrect
-            msg = 'Incorrect username/password!'
+        with open('CSVFolder/userinfo.csv', mode="r") as d:
+            reader = csv.reader(d, delimiter=',')
+            for row in reader:
+                if row == [username,password]:
+                    msg = 'Logged in Successfully!'
+                    session['loggedin'] = True
+                    session['username'] = row['username']
+                    return redirect(url_for('home'))
+                else:
+                    msg = 'Username or password is Incorrect!'
+    #Show the login form with message (if any)
     return render_template('main.html', msg='')
 # http://localhost:5000/pythinlogin/profile - this will be the profile page, only accessible for loggedin users
 @app.route('/htmlWIP/profile')
