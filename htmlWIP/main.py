bafc8749219e775f0fce692ebcb62e833e7b9865
Zack Doubikin <61536664+Megakarp@users.noreply.github.com>
2023-02-23 19:44:18 -0500

    Update main.py

--- htmlWIP/main.py
+++ htmlWIP/main.py
@@ -1,5 +1,4 @@
 from flask import Flask, render_template, request, redirect, url_for, session
-from flask_mysqldb import MySQL
 import requests
 import json
 import csv
@@ -12,14 +11,6 @@ app = Flask(__name__)
 # Change this to your secret key (can be anything, it's for extra protection)
 app.secret_key = 'your secret key'
 
-# Enter your database connection details below
-app.config['MYSQL_HOST'] = 'localhost'
-app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = ''
-app.config['MYSQL_DB'] = 'pythonlogin'
-
-# Intialize MySQL
-mysql = MySQL(app)
 
 # http://localhost:5000/htmlWIP/ - the following will be our login page, which will use both GET and POST requests
 @app.route('/htmlWIP/', methods=['GET', 'POST'])
